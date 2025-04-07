c6c2164e64fc68fff16088fdae84f4a80cc24fb9
Arwa1 <arwaalaa24682468@gmail.com>
2025-04-07 05:03:47 +0200

    Add project files.

---
+++ ECommerce.Repository/Data/ECommerceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.DashBoard.Data
+{
+    public class ECommerceDbContext:IdentityDbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
---
+++ ECommerce/Program.cs
@@ -0,0 +1,48 @@
+
+using ECommerce.DashBoard.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<ECommerceDbContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ECommerceDbContext>()
+                .AddDefaultTokenProviders();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            //builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer(); // Required for Swagger
+            builder.Services.AddSwaggerGen(); // Adds Swagger generator
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                //app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

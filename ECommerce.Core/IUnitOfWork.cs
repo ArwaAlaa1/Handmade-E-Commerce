b06b76b18a07ba7f564ab559a073e2d1c61d6251
Arwa Alaa <112275979+ArwaAlaa1@users.noreply.github.com>
2025-04-09 18:50:52 +0200

    Update IUnitOfWork.cs

--- ECommerce.Core/IUnitOfWork.cs
+++ ECommerce.Core/IUnitOfWork.cs
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 ﻿using ECommerce.Core.Repository.Contract;
-=======
 ﻿using ECommerce.Core.Models;
->>>>>>> 0804e9add3b9992e97b915c34bf6f24661df96d5
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +11,13 @@ namespace ECommerce.Core
 {
     public interface IUnitOfWork : IDisposable
     {
-<<<<<<< HEAD
+
         IGenericRepository<T> Repository<T>() where T : class;
         //IProductRepository ProductRepository { get; }
         //ICategoryRepository CategoryRepository { get; }
         void Save();
-=======
         IGenericRepository<T> Repository<T>() where T : BaseEntity;
         Task<int> SaveAsync();
->>>>>>> 0804e9add3b9992e97b915c34bf6f24661df96d5
+
     }
 }

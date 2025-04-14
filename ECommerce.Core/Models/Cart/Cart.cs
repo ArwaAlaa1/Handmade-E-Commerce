21622907890418724c8f3d1fbe60bdcf0b1bb47f
Arwa1 <arwaalaa24682468@gmail.com>
2025-04-14 09:34:19 +0200

    Add Cart Api Controller

--- ECommerce.Core/Models/Cart/Cart.cs
+++ ECommerce.Core/Models/Cart/Cart.cs
@@ -8,6 +8,18 @@ namespace ECommerce.Core.Models.Cart
 {
     public class Cart
     {
+        public Cart()
+        {
+            
+        }
+        private string cartId;
+
+        public Cart(string cartId)
+        {
+            this.cartId = cartId;
+            CartItems = new List<CartItem>();
+        }
+
         public string Id { get; set; }
         public List<CartItem> CartItems { get; set; }
     }

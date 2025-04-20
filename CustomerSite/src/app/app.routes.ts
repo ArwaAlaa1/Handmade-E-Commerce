eff813e28535812041336957d9d00502f56875f0
Arwa1 <arwaalaa24682468@gmail.com>
2025-04-20 11:28:54 +0200

    Add Cart Component in Angular site

--- CustomerSite/src/app/app.routes.ts
+++ CustomerSite/src/app/app.routes.ts
@@ -7,16 +7,17 @@ import { SendPinComponent } from './components/auth/send-pin/send-pin.component'
 import { EnterPinComponent } from './components/auth/enter-pin/enter-pin.component';
 import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CartComponent } from './components/cart/cart.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path:'register', component:RegisterComponent, title:'Register Page'},
   {path:'login', component:LoginComponent, title:'Login Page'},
   {path:'home', component:HomeComponent, title:'Home Page'},
-
+  
   {path:'sendpin', component:SendPinComponent, title:'Send Pin Page'},
   {path:'enterpin/:email/:expireAt', component:EnterPinComponent, title:'Enter Pin Page'},
   {path:'resetpassword/:email', component:ResetPasswordComponent, title:'Forget Password Page'},
-
+  {path:'cart', component:CartComponent, title:'Cart Page'},
   {path:'**', canActivate:[AuthGuard],component: NotFoundComponent , title:'NotFound Page'}
 ];

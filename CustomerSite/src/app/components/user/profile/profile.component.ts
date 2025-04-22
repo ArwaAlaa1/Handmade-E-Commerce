6ccd9532db362b68cf5bdf4f138936ff0db175e8
Arwa1 <arwaalaa24682468@gmail.com>
2025-04-23 01:37:15 +0200

    Add GetCart Logic And show In Cart page in angular site

--- CustomerSite/src/app/components/user/profile/profile.component.ts
+++ CustomerSite/src/app/components/user/profile/profile.component.ts
@@ -20,6 +20,7 @@ export class ProfileComponent implements OnInit {
     .subscribe({
       next: (response) => {
         this.userData = response;
+        console.log(this.userData);
       },
       error: (error) => {
       }

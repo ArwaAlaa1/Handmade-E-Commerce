e6f12ee6b8724aedb8681fdb3baf8373d62833a9
Arwa1 <arwaalaa24682468@gmail.com>
2025-05-01 15:48:31 +0300

    Design ui of home ,header,footer component

---
+++ CustomerSite/src/app/services/order.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  token: string = '';
+  private baseUrl = 'https://localhost:7223/api/Order';
+  isLogin: boolean = false;
+  constructor(private http: HttpClient,private _cookie:CookieService,private _auth: AuthService) {
+    // this.loadUserData();
+    this._auth.userData.subscribe({
+      next: (data) => {
+        if (data) {
+          this.token = data.token;
+        }
+      }
+    });
+  }
+  public getAuthHeaders(): HttpHeaders {
+    const storedData = this._cookie.get('userData');
+    if (!storedData) {
+      return new HttpHeaders();
+    }
+    const parsedData = JSON.parse(storedData);
+    if (parsedData && parsedData.token) {
+      return new HttpHeaders({
+        Authorization: `Bearer ${parsedData.token}`,
+      });
+    }
+    return new HttpHeaders();
+  }
+   getUserOrders(guestid?:string): Observable<any>{
+    
+      return this.http.get(`${this.baseUrl}?cartId=${guestid}`, { headers:this.getAuthHeaders()});
+    }
+}

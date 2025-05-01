865e2e416f07e01cfee0ccca08ff82bb6ce731b0
Arwa1 <arwaalaa24682468@gmail.com>
2025-05-01 18:12:23 +0300

    Show Orders For user  and filter with orderNum and Status in site

--- CustomerSite/src/app/components/order/order.component.ts
+++ CustomerSite/src/app/components/order/order.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { OrderService } from '../../services/order.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
 export class OrderComponent {
+
+  constructor(private _orderServices:OrderService) { }
+ orders:any[] = [];
+  orderDetails:any = {};
+  searchItem: string = '';
+  ngOnInit(): void {
+             this._orderServices.getUserOrders().subscribe({
+               next: (data) => {
+                 console.log("order",data);
+                 this.orders = data;
+               }, error: (err) => {
+                 console.log(err);
+               }
+              });
+             
+  }
+
+  get filteredOrders() {
+    const term = this.searchItem.toLowerCase();
+    return this.orders.filter(order =>
+      order.status.toLowerCase().includes(term) ||
+      (order.id + 1000).toString().includes(term)
+    );
+  }
+  getOrder(orderid:number){
+    this._orderServices.getOrder(orderid).subscribe({
+      next: (data) => {
+        console.log("Order Details",data);
+      }, error: (err) => {
+        console.log(err);
+      }
+     });
+  }
 
 }
